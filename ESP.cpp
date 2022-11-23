be3f3858eee395f819fcd5bb6fded9d9952c2518
Pwnmy <118749575+pwnmy@users.noreply.github.com>
2022-11-23 11:39:18 -0500

    Update ESP.cpp

--- ESP.cpp
+++ ESP.cpp
@@ -66,69 +66,69 @@ void ESP::Draw()
     //    if (bInplane)
     //        return;
 
-    for (int i = 0; i < 800; i++) // 600
-    {
-        cl_entity* pEntity = (cl_entity*)(CG_Entitys + 0x318 * i);
-        if (!pEntity)
-            continue;
-
-        int type = pEntity->type;
-        if ((type == 1 && pEntity->valid & 1) && (Settings::bSnapLinesToggle || Settings::bTextToggle)) // player
+        for (int i = 0; i < 800; i++) // 600
         {
-            auto pFnc = (*(uint32_t*)(Globals::pGameBase + 0x53389FC) ^ ((uintptr_t)pEntity + 0x38) ^ *(uint64_t*)((uintptr_t)pEntity + 0x40));
-            GetDecryptedPos_fn = (GetDecryptedPos_t)(pFnc);
+            cl_entity* pEntity = (cl_entity*)(CG_Entitys + 0x318 * i);
+            if (!pEntity)
+                continue;       
 
-            if (!pFnc || !GetDecryptedPos_fn)
-                continue;
-
-            Vector validLocation;
-            GetDecryptedPos_fn((uintptr_t)pEntity + 0x48, &validLocation);
-
-            Vector vLocalPos = Globals::GetLocalPlayerPos();
-            if ((vLocalPos - validLocation).x <= 1.f && (vLocalPos - validLocation).x >= 0.f)
-                continue;
-
-            Vector2D screenPos;
-            if (WorldToScreen(validLocation, Globals::pRefDef, &screenPos))
+            int type = pEntity->type;
+            if ((type == 1 && pEntity->valid & 1) && (Settings::bSnapLinesToggle || Settings::bTextToggle)) // player
             {
-                int fdistance = sqrt(pow(vLocalPos.x - validLocation.x, 2) + pow(vLocalPos.y - validLocation.y, 2) + pow(vLocalPos.z - validLocation.z, 2)) * 0.0254f;
+                auto pFnc = (*(uint32_t*)(Globals::pGameBase + 0x53389FC) ^ ((uintptr_t)pEntity + 0x38) ^ *(uint64_t*)((uintptr_t)pEntity + 0x40));
+                GetDecryptedPos_fn = (GetDecryptedPos_t)(pFnc);
 
-                if (fdistance > 300)
+                if (!pFnc || !GetDecryptedPos_fn)
                     continue;
 
-                ImColor color{ 20, 200, 30, 255 };
-                if (fdistance <= 100)
-                    color = { 80, 127, 255, 255 };
+                Vector validLocation;
+                GetDecryptedPos_fn((uintptr_t)pEntity + 0x48, &validLocation);
 
-                std::string sdistance = "[" + std::to_string(fdistance); sdistance.append("m]");
-                std::string text = "Player " + sdistance;
-
-                if (Settings::bTextToggle)
-                    g_Renderer->RenderText(text, ImVec2(screenPos.x - 15, screenPos.y), 18, color);
-
-                if (Settings::bSnapLinesToggle)
-                    g_Renderer->RenderLine(ImVec2(960, 1080), ImVec2(screenPos.x, screenPos.y), color);
-            }
-        }
-        else if (type == 14 && Settings::bTextToggle) // vehicle
-        {
-            Vector entpos = pEntity->location;
-            if (entpos.x < 2.f)
-                continue;
-
-            Vector2D screenPos;
-            if (WorldToScreen(entpos, Globals::pRefDef, &screenPos))
-            {
                 Vector vLocalPos = Globals::GetLocalPlayerPos();
-                int fdistance = sqrt(pow(vLocalPos.x - entpos.x, 2) + pow(vLocalPos.y - entpos.y, 2) + pow(vLocalPos.z - entpos.z, 2)) * 0.0254f;
-
-                if (fdistance > 600)
+                if ((vLocalPos - validLocation).x <= 1.f && (vLocalPos - validLocation).x >= 0.f)
                     continue;
 
-                std::string sdistance = "[" + std::to_string(fdistance); sdistance.append("m]");
-                std::string text = "Vehicle " + sdistance;
-                g_Renderer->RenderText(text, ImVec2(screenPos.x - 15, screenPos.y), 18, ImColor(80, 70, 200, 255));
+                Vector2D screenPos;
+                if (WorldToScreen(validLocation, Globals::pRefDef, &screenPos))
+                {
+                    int fdistance = sqrt(pow(vLocalPos.x - validLocation.x, 2) + pow(vLocalPos.y - validLocation.y, 2) + pow(vLocalPos.z - validLocation.z, 2)) * 0.0254f;
+
+                    if (fdistance > 300)
+                        continue;
+                    
+                    ImColor color{ 20, 200, 30, 255 };
+                    if (fdistance <= 100)
+                        color = { 80, 127, 255, 255 };
+
+                    std::string sdistance = "[" + std::to_string(fdistance); sdistance.append("m]");
+                    std::string text = "Player " + sdistance;
+
+                    if (Settings::bTextToggle)
+                        g_Renderer->RenderText(text, ImVec2(screenPos.x-15, screenPos.y), 18, color);
+
+                    if (Settings::bSnapLinesToggle)
+                        g_Renderer->RenderLine(ImVec2(960, 1080), ImVec2(screenPos.x, screenPos.y), color);
+                }
             }
+            else if (type == 14 && Settings::bTextToggle) // vehicle
+            {
+                Vector entpos = pEntity->location;
+                if (entpos.x < 2.f)
+                    continue;
+
+                Vector2D screenPos;
+                if (WorldToScreen(entpos, Globals::pRefDef, &screenPos))
+                {
+                    Vector vLocalPos = Globals::GetLocalPlayerPos();
+                    int fdistance = sqrt(pow(vLocalPos.x - entpos.x, 2) + pow(vLocalPos.y - entpos.y, 2) + pow(vLocalPos.z - entpos.z, 2)) * 0.0254f;
+
+                    if (fdistance > 600)
+                        continue;
+
+                    std::string sdistance = "[" + std::to_string(fdistance); sdistance.append("m]");
+                    std::string text = "Vehicle " + sdistance;
+                    g_Renderer->RenderText(text, ImVec2(screenPos.x-15, screenPos.y), 18, ImColor(80, 70, 200, 255));
+                }
+            }        
         }
-    }
 }
